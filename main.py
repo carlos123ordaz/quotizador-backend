22b2d4fe7f6b94c0ee71cf8cbdb1b7798d0a6a57
ORDAZ <cordaz@corsusa.com>
2025-10-30 13:57:47 -0500

    xx

--- main.py
+++ main.py
@@ -26,17 +26,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"], 
-    expose_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 app.include_router(product_router)
 app.include_router(report_router)
