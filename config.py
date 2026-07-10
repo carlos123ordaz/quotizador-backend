1f92e4da4d26edf4f9a7c78311a80c885cfae78e
Carlos Ordaz Hoyos <cordaz@corsusa.com>
2026-07-10 13:43:11 -0500

    fix: temp

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Settings:
     BITRIX24_BASE_URL = os.getenv(
         "BITRIX24_BASE_URL", "https://corsusaint.bitrix24.com/rest/4296/gzwh0355xyw5ihbc")
 
-    TEMP_FOLDER = "./temp"
+    TEMP_FOLDER = os.getenv("TEMP_FOLDER", "/tmp/quotizador")
     MAX_WORKERS = os.cpu_count() - 1 if os.cpu_count() > 1 else 1
 
     CORS_ORIGINS = [
